b014ed25d7e0714d077e0d4c499829835b25d92c
Hudson Prestidge <charcon@ihug.co.nz>
2016-09-27 13:43:11 +1300

    express server set up

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 const pug = require('pug')
+const express = require('express')
 
-const compiledFunction = pug.compileFile('template.pug');
+const app = express()
 
-console.log(compiledFunction({
-  name: 'Hudson'
-}));
+const PORT = 3000
+
+app.set('view engine', 'pug')
+
+app.listen(PORT, function(){
+  console.log('server is running and listening on port: ', PORT);
+})

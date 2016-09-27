10b6ea282cb29a45348b454c8425ffc4713f079e
Hudson Prestidge <charcon@ihug.co.nz>
2016-09-27 14:02:05 +1300

    stylesheet working

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const pug = require('pug')
 const express = require('express')
+const path = require('path')
 
 const app = express()
 
@@ -7,8 +8,10 @@ const PORT = 3000
 
 app.set('view engine', 'pug')
 
+app.use('/css', express.static(path.join(__dirname, '/stylesheets')))
+
 app.get('/', function (req, res) {
-  res.render('index', { title: 'Hey', message: 'Hello there!'});
+  res.render('index', { title: 'Pug Templating', message: 'This is pug templating!'});
 });
 
 app.listen(PORT, function(){

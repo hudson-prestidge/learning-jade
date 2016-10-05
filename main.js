6a5a49da5f299b9403fa47d453aea2477cd36cb9
Hudson Prestidge <charcon@ihug.co.nz>
2016-10-05 15:00:55 +1300

    node-sass middleware working

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const pug = require('pug')
 const express = require('express')
 const path = require('path')
+const sassMiddleware = require('node-sass-middleware')
+// const bootstrap = require('bootstrap')
 
 const app = express()
 
@@ -8,11 +10,18 @@ const PORT = 3000
 
 app.set('view engine', 'pug')
 
-app.use('/css', express.static(path.join(__dirname, '/stylesheets')))
+// app.use('/css', express.static(path.join(__dirname, '/stylesheets')))
+
+app.use('/css', sassMiddleware({
+  src: path.join(__dirname, '/sass'),
+  dest: path.join(__dirname, '/stylesheets'),
+  debug: true,
+  outputStyle: 'expanded'
+}));
 
 app.get('/', function (req, res) {
   res.render('index', { title: 'Pug Templating', message: 'This is pug templating!'});
-});
+})
 
 app.listen(PORT, function(){
   console.log('server is running and listening on port: ', PORT);
